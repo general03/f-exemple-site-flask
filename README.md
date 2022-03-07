faa642b364b1893b810622305d40ca61d8f32c9c
david <david.rigaudie@itarverne.com>
2022-03-07 11:38:20 +0100

    [FEATURE] Add flask app

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pre requis
+
+python neet to be installed on your host
+`pip install pipenv`
+`cd /path/root/site`
+`pipenv install`
+
+# Command
+
+`pipenv run flask run`
---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/other")
+def hello_other():
+    page = request.args.get('page', default = 1, type = int)
+    return f"<p>Hello, Other!</p><p>Page : {page}<:p>"
